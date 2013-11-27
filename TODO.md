d7b0a90c2bfbd6fb3b172bd69e6f157b080bcfe1
Conor Smith <conorjbsmith@gmail.com>
2013-11-27 14:28:28 +0000

    Removed code that is no longer needed
    * I deleted the mock classes from the tests directory that had been created to
      facilitate the testing of static methods.
    * I removed a large chunk of commented out code from the Uploader class.
    * I updated the TODO list

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 # To Do
 
 * Add switch to mobile game for displaying ads with no data
-
-* Typeahead for Campaign name, that fills in related campaign fields
 * Add link to Advertiser page from Ad once an existing Advertiser has been selected
+* Fix up Advertisers page with ordering, pagination, no of quizzes
--- fuel/app/classes/uploader.php
+++ fuel/app/classes/uploader.php
@@ -26,7 +26,6 @@ class Uploader
             $error_message = '';
             foreach ($this->upload_adapter->get_errors() as $file) {
                 foreach ($file['errors'] as $error) {
-                    //if ($error['error'] != $upload_class::UPLOAD_ERR_NO_FILE) {
                     if ( ! $this->upload_adapter->is_no_file_error($error)) {
                         $has_error_that_isnt_no_file_error = true;
                     }
@@ -54,46 +53,4 @@ class Uploader
 
         throw new UploaderException('No file exists for a field with the name "' . $name . '".');
     }
-
-    /*public static function process()
-    {
-        Upload::process(array(
-            'overwrite' => true,
-            'path' => DOCROOT . 'files',
-            'ext_whitelist' => array('jpg', 'wmv'),
-            'randomize' => true,
-        ));
-
-        if ( ! Upload::is_valid()) {
-            $has_error_that_isnt_no_file_error = false;
-            $error_message = '';
-            foreach (Upload::get_errors() as $file) {
-                foreach ($file['errors'] as $error) {
-                    if ($error['error'] != Upload::UPLOAD_ERR_NO_FILE) {
-                        $has_error_that_isnt_no_file_error = true;
-                    }
-                    $error_message .= '<p>Error #' . $error['error'] . ': ' . $error['message'] . '</p>';
-                }
-            }
-            if ($has_error_that_isnt_no_file_error) {
-                throw new UploaderException($error_message);
-            } else {
-                throw new UploaderNoFileException($error_message);
-            }
-        }
-
-        Upload::save();
-    }
-
-    public static function find_upload_with_field_name($name)
-    {
-        $files = Upload::get_files();
-        foreach ($files as $file_data) {
-            if ($file_data['field'] == $name) {
-                return $file_data;
-            }
-        }
-
-        throw new UploaderException('No file exists for a field with the name "' . $name . '".');
-    }*/
 }
