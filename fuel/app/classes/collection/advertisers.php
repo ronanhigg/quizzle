316f0fab1e455b1cf28afe3775210670d3515085
Conor Smith <conorjbsmith@gmail.com>
2014-06-20 15:22:10 +0000

    Ordering of advertisers in operator dashboard

--- fuel/app/classes/collection/advertisers.php
+++ fuel/app/classes/collection/advertisers.php
@@ -4,6 +4,6 @@ class Collection_Advertisers extends Collection
 {
     public static $model_class = 'Model_Advertiser';
 
-    protected static $sort_field = 'title';
+    protected static $sort_field = 'name';
     protected static $sort_order = 'asc';
 }
